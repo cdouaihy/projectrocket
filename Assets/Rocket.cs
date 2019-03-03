31540e022953b48516c60b128e5df44463bd82b5
cdouaihyucsd <cdouaihy@ucsd.edu>
2019-03-03 09:29:23 -0800

    Added Audio to ship

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
     Rigidbody rigidBody;
+    AudioSource audioSource;
     // Use this for initialization
     void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -20,8 +22,13 @@ public class Rocket : MonoBehaviour {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
-            print("Thrust!");
+            if(!audioSource.isPlaying)
+                audioSource.Play();
 
+        }
+        else
+        {
+            audioSource.Stop();
         }
         if (Input.GetKey(KeyCode.A))
         {
