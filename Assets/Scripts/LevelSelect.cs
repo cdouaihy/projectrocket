53894fd1106d32e7a137347acc52df1713889522
cdouaihyucsd <cdouaihy@ucsd.edu>
2019-08-22 22:36:19 -0700

    Started Level Selection

---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public void OnClicked(GameObject button)
+    {
+        SceneManager.LoadScene(button.name);
+    }
+}
--- Assets/Scripts/MainMenuButton.cs
+++ Assets/Scripts/MainMenuButton.cs
@@ -14,4 +14,5 @@ public class MainMenuButton : MonoBehaviour {
     {
         Application.Quit();
     }
+
 }
